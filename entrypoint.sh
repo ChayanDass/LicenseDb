445d4091ef5bb9d824d223b3d27f9601c8b5265c
Chayan Das <01chayandas@gmail.com>
2025-08-10 17:13:21 +0530

    fix docker build

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,6 @@ set -e
 populate_db="${POPULATE_DB:-true}"
 data_file="/app/licenseRef.json"
 
-printf "READ_API_AUTHENTICATION_ENABLED=false\nTOKEN_HOUR_LIFESPAN=24\nAPI_SECRET=%s\nDEFAULT_ISSUER=www.licensedb.com\n" "$(openssl rand -hex 32)" > /app/.env
-/app/init.sh
+printf "READ_API_AUTHENTICATION_ENABLED=false\nTOKEN_HOUR_LIFESPAN=24\nAPI_SECRET=%s\n" "$(openssl rand -hex 32)" > /app/.env
+/app/init.sh 
 exec /app/laas -datafile="$data_file" -populatedb=$populate_db
